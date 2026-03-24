b96b2caa9824acb97c4b3a023110a9c82fbd971a
christosanj <christopher.sanjuan.flores@gmail.com>
2026-03-23 22:35:13 -0600

    App

--- app/src/main/java/com/mexiti/catphotoapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/mexiti/catphotoapp/ui/screens/HomeScreen.kt
@@ -5,20 +5,31 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.mexiti.catphotoapp.R
+import com.mexiti.catphotoapp.model.CatPhoto
 import com.mexiti.catphotoapp.viewmodel.CatUiState
-
 
 @Composable
 
@@ -29,8 +40,7 @@ fun HomeScreen(
                ){
     when( catUiState){
         is CatUiState.Loading -> LoadingScreen(modifier = modifier.fillMaxSize())
-        is CatUiState.Success ->ResultScreen( photos = catUiState.photos,
-            modifier = modifier.fillMaxWidth())
+        is CatUiState.Success -> PhotosGridSCreen(catUiState.photos, modifier, contentPadding)
         is CatUiState.Error -> ErrorScreen(modifier =  modifier.fillMaxSize())
 
     }
@@ -45,6 +55,30 @@ fun LoadingScreen(modifier: Modifier = Modifier){
     }
 
 }
+
+
+@Composable
+fun CatPhotoCard(photo: CatPhoto,modifier: Modifier){
+
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    )
+    {
+        AsyncImage(
+            model = ImageRequest.Builder(context = LocalContext.current)
+                .data(photo.url)
+                .crossfade(true)
+                .build(),
+            contentDescription = stringResource(R.string.cat_image),
+            modifier = modifier,
+            error = painterResource(id = R.drawable.error_load),
+            placeholder = painterResource(R.drawable.loader),
+            contentScale = ContentScale.Fit
+        )
+    }
+}
+
 
 @Composable
 fun ResultScreen(photos:String, modifier: Modifier = Modifier){
@@ -64,16 +98,33 @@ fun ErrorScreen(modifier: Modifier = Modifier){
         Image(painter = painterResource(id = R.drawable.error_load)
             , contentDescription = "Error Loading" )
         Text(text = stringResource(R.string.problem_with_connection))
+        }
+    }
+
+
+@Composable
+fun PhotosGridSCreen(
+    phothos:List<CatPhoto>,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp)
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(150.dp),
+        modifier = modifier.padding(horizontal = 4.dp),
+        contentPadding = contentPadding
+    ) {
+        items(
+            items = phothos,
+            key = { photo -> photo.id }
+        ) { photo ->
+            CatPhotoCard(
+                photo = photo,
+                modifier = modifier
+                    .padding(4.dp)
+                    .fillMaxWidth()
+                    .aspectRatio(1.5f)
+            )
+        }
     }
 }
 
-@Preview
-@Composable
-fun HomeScreenPreview(){
-    Surface(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        HomeScreen(catUiState = CatUiState.Success("photos"))
-     }
-
-}
--- app/src/main/java/com/mexiti/catphotoapp/viewmodel/CatViewModel.kt
+++ app/src/main/java/com/mexiti/catphotoapp/viewmodel/CatViewModel.kt
@@ -5,12 +5,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.mexiti.catphotoapp.model.CatPhoto
 import com.mexiti.catphotoapp.network.CatApi
 import kotlinx.coroutines.launch
 import java.io.IOException
 
 sealed interface CatUiState{
-    data class Success(val photos:String) : CatUiState
+    data class Success(val photos: List<CatPhoto>) : CatUiState
     object Error: CatUiState
     object Loading: CatUiState
 }
@@ -22,14 +23,15 @@ class CatViewModel:ViewModel(){
         getCatPhotos()
     }
 
-   private  fun getCatPhotos(){
+    fun getCatPhotos(){
          viewModelScope.launch {
             catUiState = try {
                  val listResult = CatApi.retrofitService.getPhotos()
-                CatUiState.Success("Number of photos is ${listResult.size}")
-             } catch (e: IOException){
+                 CatUiState.Success(listResult)
+            }
+            catch (e: IOException){
                  CatUiState.Error
-             }
+            }
 
          }
     }
